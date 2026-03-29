8f1cd6e3ab013c836b976457269d8bf2010842ad
博主 <blog@example.com>
2026-03-29 15:04:23 +0800

    init: yukina blog

---
+++ yukina.config.ts
@@ -0,0 +1,70 @@
+import I18nKeys from "./src/locales/keys";
+import type { Configuration } from "./src/types/config";
+
+const YukinaConfig: Configuration = {
+  title: "我的博客",
+  subTitle: "记录生活与技术的点滴",
+  brandTitle: "我的博客",
+
+  description: "个人博客站点",
+
+  site: "https://example.com",
+
+  locale: "zh-CN", // set for website language and date format
+
+  navigators: [
+    {
+      nameKey: I18nKeys.nav_bar_home,
+      href: "/",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_archive,
+      href: "/archive",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_about,
+      href: "/about",
+    },
+    {
+      nameKey: I18nKeys.nav_bar_github,
+      href: "https://github.com/WhitePaper233/yukina",
+    },
+  ],
+
+  username: "博主昵称",
+  sign: "这是一段个人签名。",
+  avatarUrl: "https://s2.loli.net/2025/01/25/FPpTrQSezM8ivbl.webp",
+  socialLinks: [
+    {
+      icon: "line-md:github-loop",
+      link: "https://github.com",
+    },
+  ],
+  maxSidebarCategoryChip: 6, // It is recommended to set it to a common multiple of 2 and 3
+  maxSidebarTagChip: 12,
+  maxFooterCategoryChip: 6,
+  maxFooterTagChip: 24,
+
+  banners: [
+    "https://s2.loli.net/2025/01/25/PBvHFjr5yDu6t4a.webp",
+    "https://s2.loli.net/2025/01/25/6bKcwHZigzlM4mJ.webp",
+    "https://s2.loli.net/2025/01/25/H9WgEK6qNTcpFiS.webp",
+    "https://s2.loli.net/2025/01/25/njNVtuUMzxs81RI.webp",
+    "https://s2.loli.net/2025/01/25/tozsJ8QHAjFN3Mm.webp",
+    "https://s2.loli.net/2025/01/25/Pm89OveZq7NWUxF.webp",
+    "https://s2.loli.net/2025/01/25/UCYKvc1ZhgPHB9m.webp",
+    "https://s2.loli.net/2025/01/25/JjpLOW8VSmufzlA.webp",
+  ],
+
+  slugMode: "HASH", // 'RAW' | 'HASH'
+
+  license: {
+    name: "CC BY-NC-SA 4.0",
+    url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+  },
+
+  // WIP functions
+  bannerStyle: "LOOP", // 'loop' | 'static' | 'hidden'
+};
+
+export default YukinaConfig;
